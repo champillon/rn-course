2807af923dd36973293d6d9fed1e88c610415cf3
Chakrit Likitkhajorn <chrisza@Chakrits-MacBook-Pro.local>
2020-11-15 15:17:24 +0700

    Install prettier

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView, Button, ScrollView } from 'react-native'
+import { StyleSheet, Text, SafeAreaView, Button, ScrollView } from 'react-native'
 
 export default function App(): JSX.Element {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView horizontal={false}>
         <Text>Hello world!!</Text>
-        <Button title="Nice!" onPress={() => null}></Button>   
+        <Button title="Nice!" onPress={() => null}></Button>
         <StatusBar style="auto" />
       </ScrollView>
     </SafeAreaView>
